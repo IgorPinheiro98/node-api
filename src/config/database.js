7badfbddbd0708673fb8f0d61fc938169e0a7cbc
Igor Pinheiro <igorpinheiro2898@gmail.com>
2020-10-01 21:53:34 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+module.exports = {
+    dialect: 'postgres',
+    url: process.env.DB_URL,
+    define: {
+        timestamps: true, // created_at e updated_at
+        underscored: true,
+        underscoredAll: true,
+    },
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+};
